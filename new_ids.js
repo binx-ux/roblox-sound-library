c5604d3e460b1df40da1a850d29ee0ba8e25f8f5
binx-ux <priceanthony787@gmail.com>
2025-12-26 10:55:53 -0500

    Create new_ids.js

---
+++ new_ids.js
@@ -0,0 +1,11 @@
+// Just list your new sounds here
+module.exports = [
+  { name: "Anime Girl Laugh", id: "6389463761" },
+  { name: "Bass-Boosted Fart Noise", id: "6445594239" },
+  { name: "Boat Horn", id: "229325720" },
+  { name: "BONK", id: "8864069181" },
+  { name: "BYE BYE!", id: "7334141704" },
+  { name: "COD “Mission Failed”", id: "7361248895" },
+  { name: "Everybody in this Server…", id: "1461317727" },
+  // Add more sounds here as { name: "...", id: "..." }
+];
